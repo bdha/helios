e017a929bae811193547c5ae3ec558f1f2e73928
Andrew Thompson <andrew@hijacked.us>
2016-06-29 14:43:17 +0000

    Update bootstrap script a little

--- python-bits/helios/bootstrap.sh
+++ python-bits/helios/bootstrap.sh
@@ -6,6 +6,11 @@ HELIOSDIR="$(dirname $(readlink -f $(dirname "$0")))"
 rm -rf /opt/helium/helios/current
 ln -sf $HELIOSDIR /opt/helium/helios/current
 npm install -g smfgen
+## apparently libpython doesn't end up in the package, bleh
+pkgin install python35
+pkgin install consul
+## if we want to create home directories here, we need to make sure the base dir exists
+mkdir /var/helium
 smfgen < /opt/helium/helios/current/helios/config/service.json > /opt/helium/helios/current/helios/config/service.xml
 svccfg import /opt/helium/helios/current/helios/config/service.xml
 svcadm clear helios
