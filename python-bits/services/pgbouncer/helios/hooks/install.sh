5aac38ad3f3cc1f272450d4f82271fcf2631bb54
Andrew Thompson <andrew@hijacked.us>
2016-06-29 17:53:25 +0000

    Fix many bugs in pgbouncer service definition

--- python-bits/services/pgbouncer/helios/hooks/install.sh
+++ python-bits/services/pgbouncer/helios/hooks/install.sh
@@ -1,1 +1,10 @@
 #!/usr/bin/ksh
+
+svcadm disable pgbouncer
+svccfg delete pgbouncer
+
+mkdir /var/pgsql
+chown postgres:postgres /var/pgsql
+
+mkdir /var/log/pgbouncer
+chown postgres:postgres /var/log/pgbouncer
